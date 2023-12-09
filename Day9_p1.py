f84f274974652e11c8981b83da18f2fbd5e1f5b9
Simon Andersen <simonandersen@Simons-MBP.home>
2023-12-09 10:33:02 +0100

    day9

---
+++ Day9_p1.py
@@ -0,0 +1,23 @@
+seq = []
+with open('inputs/day9.txt') as file:
+	for x in file:
+		seq.append([int(x) for x in x.split()])
+
+def find_diff(seq):
+	diffs = []
+	for i in range(1, len(seq)):
+		diffs.append(seq[i] - seq[i-1])
+	return diffs
+sm = 0
+for s in seq:
+	t = [s]
+	dif = find_diff(s)
+	while not all([x == 0 for x in dif]):
+		t.append(dif)
+		dif = find_diff(dif)
+
+	for i in range(len(t)-2, -1, -1):
+		t[i].append(t[i][-1] + t[i+1][-1])
+	sm += t[0][-1]
+print(sm)
+		
